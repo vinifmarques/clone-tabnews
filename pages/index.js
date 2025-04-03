dc33b4410aa0ddaddc68526b6bb923ccb0d3cfc9
Vinicius Marques <86029446+vinifmarques@users.noreply.github.com>
2025-04-03 04:18:22 +0000

    alterando texto para mais um teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Oi Matheus! Seja bem-vindo. Uma noite segura a todos.</h1>;
+  return <h1>Oi Matheus! Seja bem-vindo. Uma boa noite segura a todos. 🏴‍☠️</h1>;
 }
 
 export default Home;
